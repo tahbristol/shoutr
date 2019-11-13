682850d640718987dd406c5365b1a19e5e09af7e
tahbristol <tahbristol@gmail.com>
2019-11-12 22:10:17 -0500

    photo shouts

---
+++ db/migrate/20191113024714_create_photo_shouts.rb
@@ -0,0 +1,8 @@
+class CreatePhotoShouts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :photo_shouts do |t|
+      t.attachment :image, null: false
+      t.timestamps
+    end
+  end
+end
