50a7d89a2f0d7dbe60560a3310acff94907ddc55
tahbristol <tahbristol@gmail.com>
2019-11-12 21:41:25 -0500

    null columns and user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < Clearance::UsersController
-  def show
 
+  def new
+    @user = User.new
+  end
+
+  def show
     @user = User.find(params[:id])
     @shouts = @user.shouts
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_020142) do
+ActiveRecord::Schema.define(version: 2019_11_13_024024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shouts", force: :cascade do |t|
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "content_type"
-    t.integer "content_id"
+    t.string "content_type", null: false
+    t.integer "content_id", null: false
     t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id"
   end
 
