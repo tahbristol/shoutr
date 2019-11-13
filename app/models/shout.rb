a99ca649f2bb3561d002994f7e95557df2d2e638
tahbristol <tahbristol@gmail.com>
2019-11-12 21:33:42 -0500

    added polymorphic rel with shouts

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,7 +1,7 @@
 class Shout < ApplicationRecord
-  validates :body, presence: true, length: { in: 1..144 }
   validates :user, presence: true
   belongs_to :user
+  belongs_to :content, polymorphic: true
 
   default_scope { order(created_at: :desc )}
   delegate :username, to: :user
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_013854) do
+ActiveRecord::Schema.define(version: 2019_11_13_020142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shouts", force: :cascade do |t|
-    t.string "body", null: false
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "content_type"
+    t.integer "content_id"
+    t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id"
+  end
+
+  create_table "text_shouts", force: :cascade do |t|
+    t.string "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
